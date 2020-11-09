cce5ba9b778125b0d2cbd46a08d71090e0a9d7db
pabloalonso-20 <alonso.pablo20@gmail.com>
2020-11-09 14:37:01 -0600

    add test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void test(){
+//        fail("not yet implemented");
+        assertEquals(5,5);
+    }
+
+}
